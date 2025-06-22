45fcf210f519e042f693166dab292fe74ed78a47
RirinnurFatimah <154777598+RirinnurFatimah@users.noreply.github.com>
2025-06-22 22:05:19 +0700

    Update landing.jsx

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 
 const Landing = () => {
   return (
-    <div className="min-h-screen bg-[#b2e3ea] flex flex-col items-center justify-between text-gray-800">
+    <div className="min-h-screen bg-[#A6E3E9] flex flex-col items-center justify-between text-gray-800">
       {/* Konten Utama */}
       <div className="max-w-6xl mx-auto p-6 pt-12 flex flex-col lg:flex-row items-center gap-12">
         {/* Gambar */}
